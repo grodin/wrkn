7abb3075fd7e647714470123c424783ac69c62b5
Joseph Cooper <2599295+grodin@users.noreply.github.com>
2023-06-28 22:00:23 +0100

    feat: Show recent entries <WIP>

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use crate::cli::{Cli, Command};
+use clap::Parser;
+use color_eyre::eyre;
+
+mod cli;
+mod doing;
+mod parser;
+
+fn main() -> eyre::Result<()> {
+    let args = Cli::parse();
+    println!("{:?}", args);
+    match args.command.unwrap_or(Command::Recent { count: 10 }) {
+        Command::Recent { count } => run_recent(count),
+    }
+}
+
+fn run_recent(count: u8) -> eyre::Result<()> {
+    println!("printing {} most recent entries", count);
+    Ok(())
+}
