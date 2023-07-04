310f558e281a4adef52c833689d84d0b109d5f79
Joseph Cooper <2599295+grodin@users.noreply.github.com>
2023-07-04 16:44:20 +0100

    feat: Colorize output if supported

--- src/cli/recent.rs
+++ src/cli/recent.rs
@@ -1,7 +1,11 @@
 use crate::cli::RunnableCommand;
 use crate::config::Config;
+use chrono_humanize::HumanTime;
 use clap::Args;
 use itertools::Itertools;
+use owo_colors::{OwoColorize, Stream};
+use std::io;
+use std::io::{stdout, Write};
 use wrkn::file;
 use wrkn::wrkn::{sort_entries_by_timestamp, Entry};
 
@@ -19,6 +23,22 @@ impl RunnableCommand for RecentCommand {
             .take(self.count as usize)
             .sorted_by_key(sort_entries_by_timestamp)
             .collect();
-        Ok(entries.iter().for_each(|entry| println!("{}", entry)))
+        let mut stdout = io::BufWriter::new(stdout().lock());
+        for entry in entries {
+            pretty_print(&mut stdout, &entry)?;
+        }
+        Ok(())
     }
+}
+
+fn pretty_print(writer: &mut impl Write, entry: &Entry) -> color_eyre::Result<()> {
+    let pretty_timestamp = format!(
+        "{:<14}",
+        HumanTime::from(entry.timestamp)
+            .if_supports_color(Stream::Stdout, |text| text.bright_black())
+    );
+    let pretty_separator = " | ".if_supports_color(Stream::Stdout, |text| text.cyan());
+    let pretty_title = &entry.title;
+    writeln!(writer, "{pretty_timestamp}{pretty_separator}{pretty_title}")?;
+    Ok(())
 }
