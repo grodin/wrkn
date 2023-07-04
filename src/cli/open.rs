e541fa4245343ffd935557cfc1871783db11c810
Joseph Cooper <cooper.joseph@gmail.com>
2023-07-04 21:31:55 +0100

    feat: Add open command to open wrkn file in editor

---
+++ src/cli/open.rs
@@ -0,0 +1,13 @@
+use crate::cli::RunnableCommand;
+use crate::config::Config;
+use clap::Args;
+
+#[derive(Debug, Args)]
+pub(crate) struct OpenCommand {}
+
+impl RunnableCommand for OpenCommand {
+    fn run(self, config: &Config) -> color_eyre::Result<()> {
+        edit::edit_file(&config.wrkn_file)?;
+        Ok(())
+    }
+}
