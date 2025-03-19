a3527b8cf554673ebb7124ea5d4866c0d47b4655
Joseph Cooper <cooper.joseph@gmail.com>
2025-03-19 18:10:28 +0000

    feat: Write file atomically

--- src/file.rs
+++ src/file.rs
@@ -1,27 +1,21 @@
 use crate::wrkn::{sort_entries_by_timestamp_reverse, Entry};
+use atomicwrites::{AtomicFile, OverwriteBehavior};
 use color_eyre::eyre;
-use std::fs::{File, OpenOptions};
+use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufWriter, Write};
+use std::io::{BufRead, Write};
 use std::path::Path;
 
-pub fn write_wrkn_file(mut out: impl Write, data: &mut [Entry]) -> eyre::Result<()> {
-    data.sort_by_key(sort_entries_by_timestamp_reverse);
-    for entry in data {
-        writeln!(out, "{entry}")?
-    }
-    Ok(())
-}
-
 pub fn save_wrkn_file<P: AsRef<Path>>(path: P, data: &mut [Entry]) -> eyre::Result<()> {
-    let f = BufWriter::new(
-        OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(&path)?,
-    );
-    write_wrkn_file(f, data)
+    let af = AtomicFile::new(&path, OverwriteBehavior::AllowOverwrite);
+    af.write(|f| {
+        data.sort_by_key(sort_entries_by_timestamp_reverse);
+        for entry in data {
+            writeln!(f, "{entry}")?
+        }
+        Ok::<(), io::Error>(())
+    })?;
+    Ok(())
 }
 
 pub fn read_wrkn_file<P: AsRef<Path>>(path: P) -> eyre::Result<Vec<Entry>> {
