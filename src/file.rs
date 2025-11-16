a59123dc7b401e8dc0614e73b22a7a3e85ba4f15
Joseph Cooper <cooper.joseph@gmail.com>
2025-11-16 18:36:34 +0000

    fix: Don't overwrite symlinks
    Second attempt at fixing #3.
    
    Since a3527b8c, wrkn writes files atomically. Since it doesn't check if the 
    data file is a symlink, a symlink is overwritten rather than dereferenced
    and the target written to.
    
    This change checks if the data file is a symlink, and if it is, resolves its
    target, and uses the resolved path to write to.
    
    The change also adds an integration regression test for this behaviour.
    
    Closes #3

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,36 @@
 use crate::wrkn::{sort_entries_by_timestamp_reverse, Entry};
 use atomicwrites::{AtomicFile, OverwriteBehavior};
 use color_eyre::eyre;
+use color_eyre::eyre::Context;
 use std::fs::File;
-use std::io;
 use std::io::{BufRead, Write};
 use std::path::Path;
+use std::{fs, io};
 
 pub fn save_wrkn_file<P: AsRef<Path>>(path: P, data: &mut [Entry]) -> eyre::Result<()> {
-    let af = AtomicFile::new(&path, OverwriteBehavior::AllowOverwrite);
+    let path = path.as_ref().to_path_buf();
+
+    // If path doesn't exist then we can't canonicalize it
+    // But that doesn't matter for the symlink resolving below since if it doesn't exist
+    // then it can't be a symlink
+    let path = if path.exists() {
+        path.canonicalize()?
+    } else {
+        path
+    };
+
+    // If path is a symlink, resolve the symlink so we atomically write to the target
+    let resolved_path;
+    let path = if path.is_symlink() {
+        resolved_path = fs::read_link(&path)
+            .with_context(|| format!("Failed to read link: {}", &path.display()))?
+            .canonicalize()?;
+        resolved_path
+    } else {
+        path
+    };
+
+    let af = AtomicFile::new(path, OverwriteBehavior::AllowOverwrite);
     af.write(|f| {
         data.sort_by_key(sort_entries_by_timestamp_reverse);
         for entry in data {
